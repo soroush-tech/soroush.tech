41325bfbf023012e705c267205199e0c2d392ab5
Masoud Soroush <masoud@soroush.tech>
2025-07-06 19:29:48 +0200

    feat(theme): Box Component

--- src/theme/Box.tsx
+++ src/theme/Box.tsx
@@ -1,50 +1,13 @@
-import { type Theme as DefaultTheme } from '@emotion/react'
-
 import styled from '@emotion/styled'
-
-import {
-  space,
-  layout,
-  color,
-  flexbox,
-  border,
-  typography,
-  position,
-  type SpaceProps,
-  type LayoutProps,
-  type ColorProps,
-  type FlexboxProps,
-  type BorderProps,
-  type TypographyProps,
-  type PositionProps,
-} from 'styled-system'
 import { createShouldForwardProp, props } from '@styled-system/should-forward-prop'
+import { View, type ViewProps } from 'src/theme/View.tsx'
 
-export interface BoxProps
-  extends LayoutProps<DefaultTheme>,
-    ColorProps<DefaultTheme>,
-    BorderProps<DefaultTheme>,
-    TypographyProps<DefaultTheme>,
-    PositionProps<DefaultTheme>,
-    SpaceProps<DefaultTheme>,
-    LayoutProps<DefaultTheme>,
-    FlexboxProps<DefaultTheme> {
+export interface BoxProps extends ViewProps {
   aspectRatio?: string | number
   cursor?: string
   transition?: string
-  gap?: string | number
 }
 
-const shouldForwardProp = createShouldForwardProp([...props, 'cursor', 'transform', 'gap'])
+const shouldForwardProp = createShouldForwardProp([...props, 'cursor', 'transform'])
 
-export const BaseBox = styled('div', { shouldForwardProp })<BoxProps>(
-  space,
-  layout,
-  color,
-  flexbox,
-  border,
-  typography,
-  position
-)
-
-export const Box = styled(BaseBox, { label: 'box' })<BoxProps>``
+export const Box = styled(View, { label: 'box', shouldForwardProp })<BoxProps>``
