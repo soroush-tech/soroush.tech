5b06bbb70062b14d7d3f017eea936ca3143c0e1f
Masoud Soroush <masoud@soroush.tech>
2025-07-26 09:08:59 +0200

    feat(common): Header Component

---
+++ src/common/Header.tsx
@@ -0,0 +1,35 @@
+import type { FC } from 'react'
+import { NavLink } from 'src/common/NavLink'
+import { View } from 'src/theme/View'
+import Logo from '/soroush.svg'
+import { Flex } from 'src/theme/Flex.tsx'
+
+export const Header: FC = () => {
+  return (
+    <Flex justifyContent="space-between" alignItems="center" bg="primary" px={2} py={3}>
+      <Flex justifyContent="center" alignItems="center">
+        <a href="https://soroush.tech">
+          <img src={Logo} alt="Soroush logo" />
+        </a>
+
+        <View as="nav">
+          <NavLink href="/" mx={1} p={1} color="default">
+            Home
+          </NavLink>
+          <NavLink href="/about" mx={1} p={1} color="default">
+            About
+          </NavLink>
+          <NavLink href="/projects" mx={1} p={1} color="default">
+            Projects
+          </NavLink>
+          <NavLink href="/skills" mx={1} p={1} color="default">
+            Skills
+          </NavLink>
+          <NavLink href="/contact" mx={1} p={1} color="default">
+            Contact
+          </NavLink>
+        </View>
+      </Flex>
+    </Flex>
+  )
+}
